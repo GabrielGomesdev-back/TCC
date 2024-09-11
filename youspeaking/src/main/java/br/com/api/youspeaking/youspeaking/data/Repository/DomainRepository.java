247cfc82ff3115af3d8d3c8b3f9f0ada2c3f0c51
Gabriel Gomes <gabrielgomesdev-back@gmail.com>
2024-09-11 17:39:46 -0300

    Hard Reset first commit

--- youspeaking/src/main/java/br/com/api/youspeaking/youspeaking/data/Repository/DomainRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.youspeaking.youspeaking.data.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.api.youspeaking.youspeaking.data.Entity.DomainEntity;
-
-public interface DomainRepository extends JpaRepository<DomainEntity, Long> {
-    
-}
