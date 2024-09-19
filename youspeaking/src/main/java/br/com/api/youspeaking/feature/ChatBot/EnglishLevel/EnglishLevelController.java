133a243a32306fc306753c034da22da39da60dfa
Gabriel Gomes <gabrielgomesdev-back@gmail.com>
2024-09-18 21:46:55 -0300

    adicionando websocket

--- youspeaking/src/main/java/br/com/api/youspeaking/feature/ChatBot/EnglishLevel/EnglishLevelController.java
+++ youspeaking/src/main/java/br/com/api/youspeaking/feature/ChatBot/EnglishLevel/EnglishLevelController.java
@@ -1,6 +1,7 @@
 package br.com.api.youspeaking.feature.ChatBot.EnglishLevel;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class EnglishLevelController {
     @Autowired private EnglishLevelService service;
 
     @GetMapping(value = "/first-quiz")
+    @SendTo("/question/quiz")
     public ObjectNode getQuestionsFirstQuiz() throws APIError{
         return service.getQuestionsFirstQuiz();
     }
