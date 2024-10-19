1f463effae890cf2eb3f56af1560651002feb734
Gabriel Gomes <gabrielgomesdevback@gmail.com>
2024-10-19 14:08:43 -0300

    Ajuste

--- youspeaking/src/main/java/br/com/api/youspeaking/feature/ChatBot/Classes/ClassesService.java
+++ youspeaking/src/main/java/br/com/api/youspeaking/feature/ChatBot/Classes/ClassesService.java
@@ -26,9 +26,7 @@ public class ClassesService {
     
     protected ObjectNode greetings (String login) throws APIError{
         User user = repository.findByLogin(login);
-        String greeting = """
-                          Hello I'm the 123, and I Will teach you a new language !!!
-                          In our first class we will work with reading, I will tell you some jokes and you will translate to your main language OK ?""";
+        String greeting = "Hello I'm the 123, and I Will teach you a new language !!!\n In our first class we will work with reading, I will tell you some jokes and you will translate to your main language OK ?";
         String firstMessage = !"en".equals(user.getLanguage()) ? translationService.translateText(greeting, user.getLanguage()).get("responseData").get("translatedText").asText() : greeting;
         return Utils.jsonGreetings(firstMessage.replaceAll("123", "speaking-bot"), user.getLanguage());
     }
