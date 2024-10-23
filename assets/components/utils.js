3835d1a0db27e381dcbe1912c98f49f98ed2c34f
Gabriel Gomes <gabrielgomesdevback@gmail.com>
2024-10-23 20:50:00 -0300

    Subida para a nuvem

--- assets/components/utils.js
+++ assets/components/utils.js
@@ -1,6 +1,6 @@
 var queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const urlDominioFrontend  = "http://127.0.0.1:5500/"
+const urlDominioFrontend  = "http://you-speaking.s3-website-sa-east-1.amazonaws.com/"
 const urlDominioBackend   = "http://15.228.63.234:8084/";
 const urlDominioWebsocket = "ws://15.228.63.234:8084/";
