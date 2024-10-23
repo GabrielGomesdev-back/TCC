bd1b2974f4e78040c944947368678ca4d9c2e3bd
Gabriel Gomes <gabrielgomesdevback@gmail.com>
2024-10-23 19:59:50 -0300

    Adicionando chamada aws

--- assets/components/utils.js
+++ assets/components/utils.js
@@ -2,5 +2,5 @@ var queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 const urlDominioFrontend  = "http://127.0.0.1:5500/"
-const urlDominioBackend   = "http://localhost:8084/";
-const urlDominioWebsocket = "ws://localhost:8084/";
+const urlDominioBackend   = "http://15.228.63.234:8084/";
+const urlDominioWebsocket = "ws://15.228.63.234:8084/";
